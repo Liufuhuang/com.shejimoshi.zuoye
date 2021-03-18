d9913ae347679f511afbf359e921b4530ecdb846
Liufuhuang <73649451+Liufuhuang@users.noreply.github.com>
2021-03-18 16:15:50 +0800

    Create DataOperator.java

---
+++ shejimoshi2/DataOperator.java
@@ -0,0 +1,8 @@
+package com.shejimoshi2;
+
+public class DataOperator {
+	private static CipherA cipherA() {
+		public static void setCipherA(CipherA cipherA);
+		String encrypt(String plainText);
+	}
+}
