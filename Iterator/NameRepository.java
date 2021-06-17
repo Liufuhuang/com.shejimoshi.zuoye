c8063c70f0fc5d02d46e92f1a49be802d0f3378e
LiuFuHuang <441151179@qq.com>
2021-06-17 18:21:24 +0800

    Create NameRepository.java

---
+++ Iterator/NameRepository.java
@@ -0,0 +1,32 @@
+package com.shejimoshi.Iterator;
+
+import com.shejimoshi.Observer.Observer;
+
+public class NameRepository implements Container{
+    public String names[] = {"Robert", "John" ,"Julie","Lora"};
+
+    @Override
+    public Iterator getIterator(){
+        return new NameIterator();
+    }
+
+    private class NameIterator implements Iterator{
+        int index;
+
+        @Override
+        public boolean hasNext(){
+            if(index < names.length){
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public Object next(){
+            if(this.hasNext()){
+                return names[index++];
+            }
+            return null;
+        }
+    }
+}
