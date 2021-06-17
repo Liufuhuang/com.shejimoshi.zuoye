a99884f439424fcea9c64c883eed2f32c284534a
LiuFuHuang <441151179@qq.com>
2021-06-17 23:37:26 +0800

    Create BuilderPatternDemo.java

---
+++ Builder/BuilderPatternDemo.java
@@ -0,0 +1,16 @@
+package com.shejimoshi.Builder;
+
+public class BuilderPatternDemo {
+    public static void main(String[] args) {
+        MealBuilder mealBuilder = new MealBuilder();
+        Meal vegMeal = mealBuilder.prepareVegMeal();
+        System.out.println("Veg Meal");
+        vegMeal.showItems();
+        System.out.println("Total Cost:" + vegMeal.getCost());
+
+        Meal nonVegMeal = mealBuilder.prepareNonVegMeal();
+        System.out.println("\n\nNon-VegMeal");
+        nonVegMeal.showItems();
+        System.out.println("Total Cost:" + nonVegMeal.getCost());
+    }
+}
